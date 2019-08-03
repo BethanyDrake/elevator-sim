5cba40b6a1339feadaf30089427d4e90a3bed29a
Bethany Drake <bethanyd@thoughtworks.com>
2019-08-03 23:42:07 +1000

    track highscore

---
+++ Elevator Sim/Assets/HighScoreController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class HighScoreController : MonoBehaviour
+{
+
+    public static int highscore = 0;
+
+    public Text text;
+
+    public static void UpdateHighscore(int score)
+    {
+        if (score > highscore)
+        {
+            highscore = score;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.text = "Highscore: "+ highscore;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
